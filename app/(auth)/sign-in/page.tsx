411b10e16ef813bdb39c97cf6ac9cadc58717b93
Henok <henokaragaw17@gmail.com>
2025-04-06 04:18:47 -0900

    form setup

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,89 @@
-import React from 'react'
+'use client'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import Link from "next/link"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8).max(100),
+});
+
 
 const SignIn = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          email:"",
+          password:"",
+        },
+      })
+     
+      function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+      }
+
   return (
-    <div>SignIn</div>
+    <Card className="w-full max-w-md">
+        <CardHeader className="flex flex-col justify-center items-center">
+            <CardTitle className="text-center">Sign In</CardTitle>
+            <CardDescription>
+                Welcome back! please sign in to continue
+            </CardDescription>
+        </CardHeader>
+        <CardContent>
+        <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+         <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="Johndoe@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="*********" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="cursor-pointer">Submit</Button>
+      </form>
+    </Form>
+        </CardContent>
+        <CardFooter className="flex justify-center items-center">
+            <p className="text-sm text-muted-foreground">
+                Don&rsquo;t have an account yet? {' '}
+                <Link href={'/sign-up'} className="text-primary hover:underline">
+                    Sign Up
+                </Link>
+            </p>
+        </CardFooter>
+    </Card>
   )
 }
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,106 @@
-import React from 'react'
+'use client'
 
-const SIgnUp = () => {
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import Link from "next/link"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  name: z.string().min(2).max(50),
+  email: z.string().email(),
+  password: z.string().min(8).max(100),
+});
+
+
+const SignUp = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          name: "",
+          email:"",
+          password:"",
+        },
+      })
+     
+      function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+      }
+
   return (
-    <div>SIgnUp</div>
+    <Card className="w-full max-w-md">
+        <CardHeader className="flex flex-col justify-center items-center space-y-2">
+            <CardTitle className="text-center">Sign Up</CardTitle>
+            <CardDescription>
+               Create your account to get started.
+            </CardDescription>
+        </CardHeader>
+        <CardContent>
+        <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="John doe" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="Johndoe@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="*********" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="cursor-pointer">Submit</Button>
+      </form>
+    </Form>
+        </CardContent>
+        <CardFooter className="flex justify-center items-center">
+            <p className="text-sm text-muted-foreground">
+                Already have an account yet? {' '}
+                <Link href={'/sign-in'} className="text-primary hover:underline">
+                    Sign In
+                </Link>
+            </p>
+        </CardFooter>
+    </Card>
   )
 }
 
-export default SIgnUp
+export default SignUp
